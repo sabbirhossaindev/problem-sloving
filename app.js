c965eca02b48514e949049c417f2344dfef18804
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-26 20:26:20 +0600

    some problem writing

--- app.js
+++ app.js
@@ -1,1 +1,54 @@
-console.log('hello')
+/*
+আজকের মডিউল থেকে নিচের তিনটা জিনিস মাস্ট বুঝে নিবে 
+
+১. কখন const আর কখন let দিয়ে ভেরিয়েবল ডিক্লেয়ার করতে হয় 
+
+২. টেম্পলেট স্ট্রিং এর মধ্যে ডাইনামিক ভাবে কিভাবে কোন একটা ভেরিয়েবলকে বসানো যায় 
+
+৩. একটা প্যারামিটার ওয়ালা arrow ফাংশন কিভাবে ডিক্লেয়ার করতে হয়। 
+
+.
+
+নিচের তিনটা জিনিস বুঝে নিতে পারলে ফিউচারে কাজে লাগবে 
+
+১. কিভাবে থ্রি ডট দিয়ে (...) একটা এরে এর সব এলিমেন্ট বসিয়ে দেয়া যায় 
+
+২. কোন একটা ফাংশনে এর প্যারামিটার ভ্যালু না দিলে কিভাবে ডিফল্ট প্যারামিটার দিতে হয় 
+
+৩. অনেক লাইন ওয়ালা arrow ফাংশন কিভাবে লিখতে হয়। আর কিভাবে সেটা থেকে রিটার্ন করতে হয় 
+
+ব্যস, এই টুকুই। 
+
+*/
+
+/*
+1) Write an arrow function that will take 3 parameters, will multiply
+the parameters and will return the result.
+
+2) Write the following sentence in three lines and print the result:
+I am a web developer. I love to code. I love to eat biryani.
+
+3) Write an arrow function that will take 2 parameters: One parameter
+will come from you and the other parameter will be a default
+parameter. Add these two parameters and return the result.
+
+4) Write an arrow function where it will do the following:
+a) It will take an array where the array elements will be the
+name of your friends
+b) Check if the length of each element is even, push elements
+with even length to a new array and return the result
+Print the result.
+
+5) Write an arrow function where it will do the following:
+a) Square each array element
+b) Calculate the sum of the squared elements
+c) Return the average of the sum of the squared elements
+Print the result.
+
+6) Write an arrow function where it will do the following:
+a) It will take two array inputs
+b) Combine the two arrays and assign them in a new array
+c) Find the maximum number from the new array and return the
+result
+Print the result.
+*/
