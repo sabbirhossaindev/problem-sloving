9d0b7c05722b3615a203aacf2570af735ef0fa96
sabbir hossain <mdsabbir477470@gmail.com>
2022-08-26 20:35:12 +0600

    some problem writing

--- app.js
+++ app.js
@@ -19,9 +19,41 @@
 
 ব্যস, এই টুকুই। 
 
+আজকে কোন কোন জিনিস খেয়াল করবে 
+
+১. nested অবজেক্ট (অর্থাৎ একটা অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। আবার সেই অবজেক্ট এর প্রপার্টি এর মধ্যেও যে অবজেক্ট থাকতে পারে। সেটা খেয়াল করতে হবে )
+
+২. ডট এর আগে যে প্রশ্নবোধক চিহ্ন দিয়ে যে অপশনাল চেইনিং করা যায়। সেটা একটু খেয়াল করতে হবে। 
+
+
+
+যে জিনিসগুলা এখন না লাগলেও দুই-তিন সপ্তাহ পরে লাগবে। তখন আবার এই জিনিসগুলো নিয়ে আলোচনা করবো। তবে এখন একটু দেখে রাখলে তখন ইজি লাগবে। 
+
+৩. array এর মধ্যে map কিভাবে করতে হয়। 
+
+৪. array এর মধ্যে forEach কিভাবে করে। এবং map এর forEach এর মধ্যে ডিফারেন্স কি
+
+৫. filter জিনিসটা দিয়ে কি করা হয়। ফিল্টার যে array রিটার্ন করে সেটা ফিউচারের জন্য খেয়াল রাখতে হবে 
+
+৬. find এবং filter এর মধ্যে ডিফারেন্স কি ? 
+
+৭. অবজেক্ট এর সিম্পল destructuring কিভাবে করে ?
+
+৮. array এর সিম্পল destructuring কিভাবে করে ?
+
+
+
+যে জিনিসগুলো এখন জাস্ট ধারণা রেখে দাও। বুঝতে পারলে ভালো। না বুঝতে পারলেও কোন সমস্যা নাই। আমাদের এই কোর্স এ তেমন লাগবে না। তবে মাঝে মধ্যে ইন্টারভিউতে লেগে যেতে পারে। বা কিছুদিন পরে লেগে যেতেও পারে। 
+
+৯. class কিভাবে ডিক্লেয়ার করে 
+
+১০. inheritance কি জিনিস 
+
+১১. prototypical inheritance এর কনসেপ্টটা কি (এইটা মাঝে মধ্যে ইন্টারভিউতে জিজ্ঞেস করে বসে)|
+
 */
 
-/*
+/*.................... problem part-1 ............................
 1) Write an arrow function that will take 3 parameters, will multiply
 the parameters and will return the result.
 
@@ -51,4 +83,33 @@ b) Combine the two arrays and assign them in a new array
 c) Find the maximum number from the new array and return the
 result
 Print the result.
+*/
+
+
+/*.................... problem part-2 ............................
+ 1) You have an odd array (array with odd numbers). [ 1, 3, 5, 7, 9 ].
+Now convert this array into an even array (array with even
+numbers). [ 2, 4, 6, 8, 10 ]. Do this using for loop & array.map()
+method. Hints: add one to any odd number and it will become an even
+number.
+
+2) You are given an array say: [33, 50, 79, 78, 90, 101, 30 ]. Now
+return/get all the elements which are divisible by 10 using
+array.filter() method.
+
+3) Now do the same task of question 2. But do this using array.find()
+method. Then compare the output of question 2 & question 3.
+
+4) You have an array [ 1, 9, 17, 22 ]. Add the all elements
+of this array and give output. Do this using for loop &
+array.reduce() method.
+
+5) Challenging Follow above array of objects. So, you have 3 objects as
+array element. Can you find out the total sum from here?
+
+20 + 15 + 22 = 57 . The output will be 57
+
+What are you thinking? Yeah! Do it with for loop. I know you can do it.
+But! Wait !! Wait !!! Do the same task using array.reduce() method.
+
 */
